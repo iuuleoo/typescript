f11e6128b01da9f31767e9b8698a2eb207fdfe51
Leo <leooproencaaa@gmail.com>
2025-09-24 16:52:47 -0300

    Diferença entre Type e Interface

--- index.ts
+++ index.ts
@@ -1,15 +1,31 @@
-type Person = {
+// Interface x Type
+
+interface IBaseProduct {
+    price: number
+}
+
+interface IProduct extends IBaseProduct {
     id: number,
     name: string
 }
 
-type Teacher = Person & {
-    subjects: string[]
+interface IProduct {
+    quantity: number
 }
 
-type Student = Person & {
-    age: number
+type TBaseProduct = {
+    price: number
 }
 
-let teacher: Teacher
-let student: Student
+type TProduct = TBaseProduct & {
+    id: number,
+    name: string
+}
+
+let product1: IProduct = { id: 1, name: "Produto 1", price: 500, quantity: 12}
+let product2: TProduct = { id: 2, name: "Produto 2", price: 550}
+
+type TypeString = string
+type TypeNumber = number
+
+interface X extends string {}
