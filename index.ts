e1c543b5c804b26eda349226e5d118ef839fa86c
Leo <leooproencaaa@gmail.com>
2025-09-24 15:40:20 -0300

    Verificação de tipo estático

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-const message = "Hello, Typescript!!"
+// const message = "Hello, Typescript!!"
+let message = "Hello, Typescript!!"
+
+function showMessage() {
+  console.log(message)
+}
+
+// Consigo mudar?
+message = "Typescript!"
+
+// Consigo executa?
+// message()
+showMessage()
+
+const user = {
+  name: "Leonardo",
+  email: "Leonardo@email.com"
+}
+
+// Propriedade existe?
+//console.log(user.avatar)
+console.log(user.name)
